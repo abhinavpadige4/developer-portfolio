fdd94e3a78c36d7de60ccf935af56a481bf28292
abhinav <abhinavpadige06@gmail.com>
2026-09-04 13:07:02 +0530

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import About from './components/About.jsx'
+import Skills from './components/Skills.jsx'
+import Projects from './components/Projects.jsx'
+import Experience from './components/Experience.jsx'
+import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-darker text-slate-200">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Experience />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
